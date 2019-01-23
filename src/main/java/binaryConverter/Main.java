d0edceca2f63aead8aef9b98c5c5aacc96e6a41b
Treliusz <vmtr@interia.pl>
2019-01-23 23:31:16 +0100

    Initial Commit

---
+++ src/main/java/binaryConverter/Main.java
@@ -0,0 +1,8 @@
+package binaryConverter;
+
+public class Main {
+    public static void main(String[] args) {
+        Decimal decimal = new Decimal();
+        System.out.println(decimal.convert(2));
+    }
+}
